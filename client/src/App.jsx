0df4a480e0bfeedb92b2c904d6c01b020772f01b
Sayali <saykaw16@gmail.com>
2023-09-25 00:07:22 +0530

    fetched data and displayed it using google book api

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import HomePage from './Components/HomePage'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <HomePage/>
+    </>
+  )
+}
+
+export default App
