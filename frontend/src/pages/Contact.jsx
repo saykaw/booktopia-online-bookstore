4bcc154648f00ee336de9370e8689e77ce79bb83
Sayali <saykaw16@gmail.com>
2023-09-28 23:32:22 +0530

    pages

---
+++ frontend/src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/BaseLayout/Layout'
+
+const Contact = () => {
+  return (
+    <Layout> 
+    <h1>Contact</h1>
+</Layout>
+  )
+}
+
+export default Contact
