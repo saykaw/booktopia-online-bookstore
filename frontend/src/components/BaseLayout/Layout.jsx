70ce835f3804aa8d70bada21634ed9f9a7c24a2e
Sayali <saykaw16@gmail.com>
2023-09-30 13:56:08 +0530

    seo functionality

--- frontend/src/components/BaseLayout/Layout.jsx
+++ frontend/src/components/BaseLayout/Layout.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
-const Layout = ({children}) => {
+import {Helmet} from 'react-helmet'
+const Layout = ({children,title,description,keywords,author}) => {
   return (
     <>
     <div>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <meta name="description" content={description}/>
+      <meta name="keywords" content={keywords}/>
+      <meta name="author" content={author}/>
+      <title>{title}</title>  
+    </Helmet>
         <Header/>
         <main style ={{minHeight : "80vh"}}>{children}</main>
         <Footer/>
     </div>
     </>
   )
+}
+
+Layout.defaultProps = {
+  title : "Booktopia - Find a book that matches you",
+  description : "Mern Stack Ecommerce-Bookstore",
+  keywords : "mern,react,node,express,vite,html,css,bookstore,online-bookstore,ecommerce-bookstore,mongodb",
+  author: "ilayas"
+
 }
 
 export default Layout;
