fc34470057d4d19477b928df09c298cd6658788a
mehk <mehvish2803@gmail.com>
2023-10-01 21:06:41 +0530

    Updated header and footer components, added Contact page, and new CSS files

--- frontend/src/components/BaseLayout/Header.jsx
+++ frontend/src/components/BaseLayout/Header.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import { NavLink,Link } from 'react-router-dom'
-import {BiBookReader} from 'react-icons/bi';
-
-
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { BiBookReader } from 'react-icons/bi';
+import './Header.css'; // Import the CSS file
 
 const Header = () => {
   return (
-    <>
     <div className="navbar">
       <ul>
-        <li className="navbar-elements"><Link to='/'><BiBookReader/>Booktopia</Link></li>
-        <li className="navbar-elements"><NavLink to='/'>Home</NavLink></li>
-        <li className="navbar-elements"><NavLink to='/category'>Category</NavLink></li>
-        <li className="navbar-elements"><NavLink to='/register'>Register</NavLink></li>
-        <li className="navbar-elements"><NavLink to='/login'>Login</NavLink></li>
-        <li className="navbar-elements"><NavLink to='/cart'>Cart(0)</NavLink></li>
+        <li className="navbar-elements">
+          <Link to='/'>
+            <BiBookReader /> Booktopia
+          </Link>
+        </li>
+        <li className="Navbar_Elements">
+          <li className="nav-ele"><NavLink to='/'>Home</NavLink></li>
+          <li className="nav-ele"><NavLink to='/category'>Category</NavLink></li>
+          <li className="nav-ele"><NavLink to='/register'>Register</NavLink></li>
+          <li className="nav-ele"><NavLink to='/login'>Login</NavLink></li>
+          <li className="nav-ele"><NavLink to='/cart'>Cart(0)</NavLink></li>
+        </li>
       </ul>
     </div>
-    </>
-  )
+  );
 }
 
-export default Header
+export default Header;
