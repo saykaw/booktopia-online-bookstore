bd3e0f701e6e1afc0baad89847b281aca0ad0569
Sayali <saykaw16@gmail.com>
2023-10-02 16:00:55 +0530

    name of the logged-in user displayed with a dropdown

--- frontend/src/components/BaseLayout/Header.jsx
+++ frontend/src/components/BaseLayout/Header.jsx
@@ -3,6 +3,7 @@ import { NavLink,Link } from 'react-router-dom'
 import {BiBookReader} from 'react-icons/bi';
 import { useAuth } from '../../context/auth';
 import toast from 'react-hot-toast';
+import './Header.css'
 
 
 
@@ -28,7 +29,18 @@ const Header = () => {
           !auth.user ? (<><li className="navbar-elements"><NavLink to='/register'>Register</NavLink></li>
           <li className="navbar-elements"><NavLink to='/login'>Login</NavLink></li></>) 
           : 
-          (<><li className="navbar-elements"><NavLink onClick = {handleLogout} to='/login'>Logout</NavLink></li></>)
+          (
+          <>
+          <div className="dropdown">
+          <button className="dropdown-button">{auth?.user?.name}</button>
+            <div className="dropdown-content">
+                <li><NavLink to='/dashboard'>Dashboard</NavLink></li>
+                <li><NavLink onClick = {handleLogout} to='/login'>Logout</NavLink></li>
+            </div>
+          </div>
+          {/* <li className="navbar-elements"><NavLink onClick = {handleLogout} to='/login'>Logout</NavLink></li> */}
+          </>
+          )
         }
         <li className="navbar-elements"><NavLink to='/cart'>Cart(0)</NavLink></li>
       </ul>
