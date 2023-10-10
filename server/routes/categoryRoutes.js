9ac1ce0c507a32d0275056b53113b1fe6911e97d
Sayali <saykaw16@gmail.com>
2023-10-10 20:22:52 +0530

    api crud via backend : done

--- server/routes/categoryRoutes.js
+++ server/routes/categoryRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { isAdmin, requireSignIn } from '../middleware/authMiddleware.js';
-import { createCategoryController , getCategoryController, updateCategoryController, viewCategoryController} from '../controller/categoryController.js';
+import { createCategoryController , deleteCategoryController, getCategoryController, updateCategoryController, viewCategoryController} from '../controller/categoryController.js';
 
 //router object
 const router = express.Router();
@@ -15,7 +15,10 @@ router.put('/update-category/:id',requireSignIn,isAdmin,updateCategoryController
 router.get('/view-categories',viewCategoryController)
 
 //view a single category
-router.get('/get-category',getCategoryController)
+router.get('/get-category/:slug',getCategoryController)
+
+//delete category
+router.delete('/delete-category/:id',requireSignIn,isAdmin,deleteCategoryController)
 
 
 export default router
