b24a6fc5020f5efb60b705f59f4284b889591644
Sayali <saykaw16@gmail.com>
2023-10-10 20:23:40 +0530

    api crud via backend : done

--- server/controller/categoryController.js
+++ server/controller/categoryController.js
@@ -83,7 +83,12 @@ export const viewCategoryController = async(req,res) =>{
 export const getCategoryController = async(req,res) => {
     try{
 
-
+        const category = await categoryModel.findOne({slug:req.params.slug})
+        res.status(200).send({
+            success:true,
+            message:'Specified category viewed successfully',
+            category,
+        })
     }
     catch{
         console.log(error)
@@ -96,4 +101,23 @@ export const getCategoryController = async(req,res) => {
 
 }
 
+//delete category
+export const deleteCategoryController = async(req,res) => {
+    try{
+        const {id} = req.params;
+        await categoryModel.findByIdAndDelete(id);
+        res.status(200).send({
+            success:true,
+            message:'Category deleted successfully'
+        })
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).send({
+            success:false,
+            message:'Error by creating category',
+            error
+        })
 
+    }
+}
