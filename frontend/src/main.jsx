4c73682c59f53e4f38a6706b9dee616c56319600
Sayali <saykaw16@gmail.com>
2023-10-12 20:08:28 +0530

    api crud category -  frontend+backend both  done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'antd/dist/reset.css'
 import {BrowserRouter} from 'react-router-dom';
 import { AuthProvider } from './context/auth.jsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
