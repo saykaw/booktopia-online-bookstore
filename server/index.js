c84ff13d860ffd99f37915deb0d7346c90cb41c5
Sayali <saykaw16@gmail.com>
2023-10-15 00:35:28 +0530

    removed comments

--- server/index.js
+++ server/index.js
@@ -31,52 +31,6 @@ app.get('/',(req,res)=>{
     res.send('hello world');
 })
 
-// app.post('/api/register', async (req,res)=>{
-//     console.log(req.body)  //undefined : not receiving the body : route does not know that we are passing a json
-//     // res.json({status:'ok'})
-//     try{
-//         const newPassword = await bcrypt.hash(req.body.password,10)
-//         await User.create({
-//             name : req.body.name,
-//             email: req.body.email,
-//             password: newPassword,
-//         });
-//         res.json({status:'ok'})
-
-//     }catch(error){
-//         res.json({status:'error',message:error})
-//     }
-// })
-
-// app.post('/api/login', async (req,res)=>{
-//     const user = await User.findOne({
-//             email: req.body.email,
-//             // password: req.body.password,
-//     });
-
-//     if(!user){
-//         return {status:'error',error:'invalid login'}
-//     }
-//     const isPasswordValid = await bcrypt.compare(req.body.password, user.password)
-
-//     //if user exists we can send jwt
-//     if (user){
-
-//         const token = jwt.sign(
-//             {
-//             name : user.name,
-//             email : user.email,
-//            },
-//            'secret123'
-//         )
-//         return res.json({status:'ok',user:token});
-
-//     }
-//     else{
-//         return res.json({status:'error',user:false});
-//     }
-// })
-
 app.listen(PORT,()=>{
     console.log(`The server has connected on port ${PORT}`);
 })
