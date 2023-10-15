dd58616a5ee24125bd7eb93bdea3df5fc5b4650f
Sayali <saykaw16@gmail.com>
2023-10-15 21:06:35 +0530

    user profile page:done

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {registerController,loginController,testController,forgotPasswordController} from '../controller/authController.js'
+import {registerController,loginController,testController,forgotPasswordController, updateProfileController} from '../controller/authController.js'
 import {requireSignIn,isAdmin} from '../middleware/authMiddleware.js';
 
 //router object
@@ -30,4 +30,5 @@ router.get("/admin-auth",requireSignIn,isAdmin,(req,res)=>{
     res.status(200).send({ok:true}) 
 })
 
-
+//update profile
+router.put('/profile',requireSignIn,updateProfileController)
